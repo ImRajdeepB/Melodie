32ad86fa3441ae1e93b9c6467f9ed9e161bf7f28
Rajdeep Bharati <rajdeepbharati13@gmail.com>
2019-01-22 18:42:18 +0530

    Create AppUser table

--- recommend/models.py
+++ recommend/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class AppUser(models.Model):
+    user = models.OneToOneField(
+        User, related_name='user', on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20, default='', blank=True)
+
+    def __str__(self):
+        return self.user.username
