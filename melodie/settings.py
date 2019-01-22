fe3af5e7b94defaed619afc930e89caef080d347
Rajdeep Bharati <rajdeepbharati13@gmail.com>
2019-01-22 18:55:29 +0530

    Enabled djongo

--- melodie/settings.py
+++ melodie/settings.py
@@ -77,10 +77,8 @@ WSGI_APPLICATION = 'melodie.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'ENGINE': 'djongo',
-        # 'NAME': 'melodie-db',
+        'ENGINE': 'djongo',
+        'NAME': 'melodie-db',
     }
 }
 
